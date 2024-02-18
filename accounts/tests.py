8512bb8cec982e74b41dca6fa856133e8b825310
pathlearn3639 <pathlearn3639@gmail.com>
2024-02-18 17:36:41 +0530

    user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 class UsersManagesTests(TestCase):
@@ -30,3 +31,30 @@ class UsersManagesTests(TestCase):
             self.assertTrue(admin_user.is_active)
             self.assertTrue(admin_user.is_staff)
             self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser2",
+                "email": "testuser2@gmail.com",
+                "password1": "Gr0w5h$$",
+                "password2": "Gr0w5h$$",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser2")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser2@gmail.com")
